aa62c17621b2c10a46b833bbc83b7989adbe817a
Guillaume Gautier <guillaume.gautier@univ-lille1.fr>
2018-07-23 10:51:35 +0200

    Fix Travis #1 thx to @guillep

--- scripts/ensure_latex.sh
+++ scripts/ensure_latex.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # From https://github.com/y-yu/install-tex-travis/blob/master/install-tex.sh
-set -e
+
+#Enable to not exit on error
+#set -o errexit
+
+#Enable to trace bash execution
+#set -o xtrace
 
 DIRNAME=tl-`date +%Y_%m_%d_%H_%M_%S`
 
@@ -34,7 +39,7 @@ else
   export PATH=$PATH:$HOME/texlive/2017/bin/x86_64-linux
 fi
 
-./install-tl -profile ./small.profile -repository http://ctan.sharelatex.com/tex-archive/systems/texlive/tlnet
+./install-tl -profile ./small.profile
 tlmgr init-usertree
 tlmgr install latexmk
 tlmgr install luatex85
