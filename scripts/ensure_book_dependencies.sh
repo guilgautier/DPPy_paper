8b50e0ea7073232d5799e63e7257a4376ce664d2
Guillaume Gautier <guillaume.gautier@univ-lille1.fr>
2018-07-23 11:45:53 +0200

    Try fix inputenc with ucs

--- scripts/ensure_book_dependencies.sh
+++ scripts/ensure_book_dependencies.sh
@@ -7,9 +7,8 @@ tlmgr install doublestroke
 tlmgr install fontawesome 	% For GitHub icon
 tlmgr install graphicx
 
+tlmgr install ucs
 tlmgr install inputenc
 tlmgr install layout
-tlmgr install natbib
 tlmgr install pgffor
 tlmgr install todonotes
-tlmgr install wasysym
