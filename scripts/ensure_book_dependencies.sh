be76387e24e66ad62ff62ce2466f84822d5c23d1
Guillaume Gautier <guillaume.gautier@univ-lille1.fr>
2018-07-23 11:06:42 +0200

    Update latex packages to be installed

--- scripts/ensure_book_dependencies.sh
+++ scripts/ensure_book_dependencies.sh
@@ -1,14 +1,16 @@
-# Install packages required by the latex book template
+# Install packages
 
-tlmgr install fira
-tlmgr install gentium-tug
-tlmgr install opensans
+tlmgr install amsmath
+tlmgr install array
+tlmgr install bm
+tlmgr install dirtytalk
+tlmgr install dsfont
+tlmgr install fontawesome 	% For GitHub icon
+tlmgr install graphicx
 
-tlmgr install varwidth
-tlmgr install footmisc
-tlmgr install tcolorbox
-tlmgr install environ
-tlmgr install trimspaces
-tlmgr install ctablestack
-tlmgr install import
-tlmgr install multirow
+tlmgr install inputenc
+tlmgr install layout
+tlmgr install natbib
+tlmgr install pgffor
+tlmgr install todonotes
+tlmgr install wasysym
