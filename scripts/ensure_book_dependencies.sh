046a6be1971dd439a9fedfff300c58ab76afc487
Guillaume Gautier <guillaume.gautier@univ-lille1.fr>
2018-08-04 21:38:19 +0200

    Try install previous packages.sty with tlmgr install..

--- scripts/ensure_book_dependencies.sh
+++ scripts/ensure_book_dependencies.sh
@@ -5,10 +5,12 @@ tlmgr install bm
 tlmgr install dirtytalk
 tlmgr install doublestroke
 tlmgr install fontawesome 	% For GitHub icon
+tlmgr install framed
+tlmgr install fvextra
 tlmgr install graphicx
-
+tlmgr install ifplatform
 tlmgr install ucs
 tlmgr install layout
 tlmgr install pgffor
-#tlmgr install todonotes
 tlmgr install minted # highlight code
+tlmgr install xstring
