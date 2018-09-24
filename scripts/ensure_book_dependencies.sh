d4b9bdbeb3ea9452277a71d89aa5e5eaafe3dd7f
Guillaume Gautier <guillaume.gautier@univ-lille1.fr>
2018-09-24 14:48:00 +0200

    clean tlmgr installs

--- scripts/ensure_book_dependencies.sh
+++ scripts/ensure_book_dependencies.sh
@@ -1,16 +1,16 @@
 # Install packages
 
-tlmgr install array
-tlmgr install bm
-tlmgr install dirtytalk
+# tlmgr install array
+#tlmgr install bm
+# tlmgr install dirtytalk
 tlmgr install doublestroke
 tlmgr install fontawesome # For GitHub icon
-tlmgr install framed
+# tlmgr install framed
 tlmgr install fvextra
-tlmgr install graphicx
+# tlmgr install graphicx # part of graphics
 tlmgr install ifplatform
-tlmgr install ucs
-tlmgr install layout
-tlmgr install pgffor
+tlmgr install ucs 
+# tlmgr install layout
+# tlmgr install pgffor # part of pfg
 # tlmgr install minted # highlight code
-tlmgr install xstring
+# tlmgr install xstring
